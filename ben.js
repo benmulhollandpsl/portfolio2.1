aa7c1c8236df89856ac0b52318ef8324b90bb619
Ben Mulholland <ben.mulholland@gmail.com>
2020-04-22 21:00:19 -0400

    added ambience

--- ben.js
+++ ben.js
@@ -4,7 +4,7 @@
       alert("Please send me an email to request phone number");
 };
 
-     
+  
 // songs that will play in an array
 var songs = [
     "quaranrelax.mp3","spiral shell.mp3"
@@ -13,7 +13,7 @@ var songs = [
 
 //array of posters
 
-var posters = [
+var poster = [
     "quaranrelaxcover.jpg", "spiralshellcover.jpg"
 ];
 
@@ -24,10 +24,60 @@ var fillBar = document.getElementById("fill");
 var song = new Audio();
 var currentSong = 0;  // starts on current song
 
-window.onload = playSong;   // calls function playSong when window loads
+window.onload = playSong;   
+// calls function playSong when window loads 
 
 function playSong(){
     song.src = songs[currentSong];  //sets the source of the 0th song
     songTitle.textContent = songs[currentSong]; //set the title of song
 
+    song.play();// this is breaking cannot autoplay
 }
+
+function playOrPauseSong(){
+    if(song.paused){
+        song.play();
+        $("#play img").attr("src","./assets/audio/pause.png")
+    }
+    else{
+        song.pause();
+        $("#play img").attr("src","./assets/audio/play.png")
+        // check src
+
+    }
+}
+song.addEventListener('timeupdate',function(){
+    var position = song.currentTime / song.duration;
+
+    fillBar.style.width = position * 100 + '%';
+});
+
+//next song button
+function next(){
+    currentSong++;
+    if(currentSong > 2){
+        currentSong = 0;
+    
+    }
+    playSong();
+    $("#play img").attr("src","./assets/audio/pause.png");
+    $("#imageAlbum img").attr("src",poster[currentSong]); //should change background poster
+    $("#bg img").attr("src",poster[currentSong]);
+
+}
+//previous button
+function prev(){
+    currentSong--;
+    if(currentSong < 0){
+        currentSong = 2;
+    
+    }
+    playSong();
+    $("#play img").attr("src","./assets/audio/pause.png");
+    $("#imageAlbum img").attr("src",poster[currentSong]); 
+    $("#bg img").attr("src",poster[currentSong]);
+
+}
+
+//calling next on button click 
+//posters need to be set to each song
